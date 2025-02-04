2eaea0b675f25392ddfed4979503b99962a2f7b5
Neeraj Nishad <neeraj007ro@gmail.com>
2025-02-04 02:43:42 +0000

    variables

---
+++ 01_basics/variables.js
@@ -0,0 +1,19 @@
+ const accountId = 1234;
+ let accountEmail ="Neeraj@hook.com";
+ var accountPassword = "28427";
+ accountCity = "gorakhpur";
+let accountState;
+//  accountId = 1245    //not allowed
+
+accountEmail= "dheeraj@google.com";
+accountPassword="9438";
+accountCity = "locknow";
+
+// console.log(accountId);
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
