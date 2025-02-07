e852f51713f4b8726febefb0f1bd060f3163beb0
Neeraj Nishad <neeraj007ro@gmail.com>
2025-02-07 11:05:00 +0000

    02_datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,35 @@
+"use strict"; // teat all js code as newer version
+
+// alert(3+5)  we are using nodejs not brower
+
+
+console.log(3+
+    3) // code readability should be high
+
+
+    let name = "Neeraj"
+    let age = 20
+    let isLoggedIn = false
+     let state;
+
+
+    /* number => 2 to power 53 
+     bigint
+     string =>""
+     boolean => ture/false
+     null => standalone value
+     undefined =>
+        symbol => unique */
+
+    //  object
+
+
+    console.log(typeof name );
+    console.log(typeof age);
+    console.log(typeof state);
+    console.log(typeof null);     // => object
+    console.log(typeof undefined);// =>undefined
+    
+    
+    
+    
