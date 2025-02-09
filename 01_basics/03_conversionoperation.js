c2b641c2502cf90b7c5fc21e78e8463a1f562238
Neeraj Nishad <neeraj007ro@gmail.com>
2025-02-09 13:59:37 +0000

    operations

--- 01_basics/03_conversionoperation.js
+++ 01_basics/03_conversionoperation.js
@@ -1,7 +1,7 @@
 let score =true;
 
-console.log(typeof score);
-console.log(typeof (score));
+// console.log(typeof score);
+// console.log(typeof (score));
 
 let valueInNumber = Number (score)
 
@@ -31,8 +31,45 @@ let number = 33;
 
 let stringNumber = String(number);
 
-console.log(stringNumber)
-console.log(typeof stringNumber);
+// console.log(stringNumber)
+// console.log(typeof stringNumber);
 
 
- 
+//  ***********Operations****************
+
+let value =3;
+
+let negValue =-value
+
+// console.log(negValue);
+
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2/2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2%3);
+
+
+
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log("1"+2+3); 
+// console.log(1+2+"2");  
+
+
+// console.log((3 + 4) * 5 % 3); //this is a good practice 
+
+// console.log(+true);
+
+
+
+let gameNumber = 100
+ gameNumber++;
+
+console.log(gameNumber);
+
+
+
+
