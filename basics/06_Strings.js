df115866083b2379bbfadd84462e2702a039b32f
Neeraj Nishad <neeraj007ro@gmail.com>
2025-02-21 10:52:39 +0000

    strings

---
+++ basics/06_Strings.js
@@ -0,0 +1,54 @@
+ const name = 'hitesh' 
+ const  repoCount = 50 
+//  console.log(name + repoCount + 'sharma'); out dated
+
+// console.log(`hello my name is ${name} and my repo Count is ${ repoCount}`);
+
+const gameName = new String('Neeraj-hc')
+  
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(5));
+// console.log(gameName.indexOf('h'));
+ 
+
+const newString = gameName.substring(0 , 5)
+console.log(newString);
+
+
+const anotherString = gameName.slice(-7,5)
+console.log(anotherString);
+
+
+
+const newString1 = "    Neeraj    "
+
+console.log(newString1);
+console.log(newString1.trim());
+
+
+const url = "https://neeraj.com/hitesh%20choudhary"
+ console.log(url.replace('%20','-'));
+
+
+ console.log(url.includes('Neeraj'));
+ 
+ 
+
+console.log(gameName.split('-'));
+
+
+
+
+
+
+
+
+
+
+
+
+ 
