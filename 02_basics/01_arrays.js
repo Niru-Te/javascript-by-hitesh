407470e0a06df5957c33afc5847f87f3b46fd44c
Neeraj Nishad <neeraj007ro@gmail.com>
2025-02-23 02:02:36 +0000

    arrays

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,44 @@
+//array
+
+
+const myArray = [0,3,5,3,3,]
+const myHeroes = ["Neeraj","manish"]
+
+const myArray2 = new Array(1,3,5,3,5)
+
+// console.log(myArray[2]);
+// console.log(myArray2);
+
+// Arrays Method
+
+// myArray.push(4)
+// myArray.pop()
+
+// myArray.unshift(9)
+// myArray.shift()
+
+//  console.log(myArray.includes(9));
+//  console.log(myArray.indexOf(5));
+ 
+ const newArray = myArray.join()
+
+// console.log(myArray);
+// console.log(newArray);
+// console.log(typeof newArray);
+
+
+ console.log("A", myArray);
+
+ const myn1 = myArray.slice(1,3)
+ console.log(myn1);
+ console.log("B", myArray);
+
+ const myn2 = myArray.splice(1,3)
+ console.log(myn2);
+ 
+ console.log("c", myArray);
+ 
+ 
+ 
+
+
