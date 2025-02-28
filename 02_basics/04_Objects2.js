028c7fbe39cd73a2e9c416a7402a72c66f91ea1e
Neeraj Nishad <neeraj007ro@gmail.com>
2025-02-28 03:03:12 +0000

    Obj2

---
+++ 02_basics/04_Objects2.js
@@ -0,0 +1,78 @@
+// const tinderUser = new Object()  //singaltan object 
+
+const tinderUser = {}    // non singaltan object;
+
+tinderUser.id = "321abc"
+
+tinderUser.name = "Sammy"
+tinderUser.isLoggedIn = false
+
+
+// console.log(tinderUser);
+
+const regularUser = {
+    email:"neeraj@google.com",
+    fullname:{
+        username:{
+            firstname:"Neeraj",
+            lastname:"Nishad"
+        }
+    }
+}
+
+// console.log(regularUser.fullname.username.lastname);   // Nested Objects
+
+
+
+const obj1 = {1: "a" ,2:"b"}
+const obj2 = {3: "a" ,4:"b"}
+const obj3 = {5: "a" ,6:"b"}
+
+
+
+// const  obj4 = Object.assign({},obj1,obj2,obj3) //firt method to merg obj
+
+const obj4 = {...obj1,...obj2,...obj3}//  second method to merg obj
+// console.log(obj4);
+
+
+
+const users = [
+     {
+        id:1,
+        email:"user1@google.com"
+     },
+     {
+        id:1,
+        email:"user1@google.com"
+     },
+    
+     {
+        id:1,
+        email:"user1@google.com"
+     }
+    
+]
+
+
+
+console.log(users[1].email);
+
+
+console.log(tinderUser);
+
+
+console.log(Object.keys(tinderUser));
+console.log(Object.values(tinderUser));
+console.log(Object.entries(tinderUser));
+
+
+console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+console.log(tinderUser.hasOwnProperty('isLogged'));
+
+
+
+
+
+
+
