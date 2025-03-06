e42f890c36f8fcbd66803e1fcbf53449d97ff468
Neeraj Nishad <neeraj007ro@gmail.com>
2025-03-06 03:12:25 +0000

    Arrow

---
+++ 03_basics/03_Arrow.js
@@ -0,0 +1,38 @@
+const user = {
+    username:"Neeraj",
+    price :999,
+    message:function(){
+        console.log(`${this.username},welcome t0 website `);
+        // console.log(this);         // this  use for current context(value)
+        
+        
+    }
+}
+
+// user.message()
+// user.username = "sam"
+// user.message()
+// console.log(this);        // returns {} (empty object)
+
+
+// function chai(){
+//       let username = "hitesh"
+//     console.log(this.username);     // returns undefined
+    
+// }
+// chai()
+
+// Arrow function   () => {}
+
+
+    // const addTwo = (num1,num2) => {           
+    //     return num1 + num2                              // Explisit Arrow function
+    // }  
+
+
+    // const addTwo = (num1,num2) =>  num1 + num2        or                               // Implisit Arrow function
+    // const addTwo = (num1,num2) => ( num1 + num2 )                                      // Implisit Arrow function
+    const addTwo = (num1,num2) => ( {username:"hitesh"}                                   // Implisit Arrow function
+    
+
+    console.log(addTwo(3,5))
