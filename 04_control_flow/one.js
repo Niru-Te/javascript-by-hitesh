7f43758a1fce3528bbf5c480d7eb6037903ecf15
Neeraj Nishad <neeraj007ro@gmail.com>
2025-03-11 06:35:54 +0000

    control flow

--- 04_control_flow/one.js
+++ 04_control_flow/one.js
@@ -56,3 +56,5 @@ if(isLoggedInFromEmail || isLoggedInFromGoogle){
 }
 
 
+
+
---
+++ 04_control_flow/switch.js
@@ -0,0 +1,52 @@
+// switch statement
+
+const month = 2
+
+switch (month) {
+    case 1:
+        console.log("jan");
+        
+        break;
+    case 2:
+        console.log("fab");
+
+        break;
+    case 3:
+        console.log("march");
+
+        break;
+    case 4:
+        console.log("april");
+
+        break;
+
+    default:
+        console.log("None of these");
+        
+        break;
+}
+const month2 = "march"
+
+switch (month2) {
+    case "jan":
+        console.log("jan");
+        
+        break;
+    case "fab":
+        console.log("fab");
+
+        break;
+    case "march":
+        console.log("march");
+
+        break;
+    case "april":
+        console.log("april");
+
+        break;
+
+    default:
+        console.log("None of these");
+        
+        break;
+}
