13a35ba2c372bd5e7f9e868afe4e5a6c057c8a38
Neeraj Nishad <neeraj007ro@gmail.com>
2025-03-12 02:34:01 +0000

    dowhile loop

---
+++ 05_iterations/two.js
@@ -0,0 +1,28 @@
+// do while loops 
+
+let index = 0
+
+while (index <= 10) {
+    //  console.log(`value of the index is ${index}`);
+     index = index +2
+    
+}
+
+let myArray = ["superman", "batman ","spiderman"]
+
+let arr = 0
+while(arr<myArray.length){
+    // console.log(`this is the length of arr ${myArray[arr]}`);
+      arr = arr +1 
+}
+
+// in dowhile loop before this is exicute and then after condition check
+
+let score = 11
+
+do {
+    
+    console.log(`score is ${score}`)
+    score++   
+
+} while (score <=10);
